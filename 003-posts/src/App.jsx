37d58ce3bfb5fa835360dfe6cec9d40d86ebfa3d
ahmedragab <ahmedragab20901@gmail.com>
2023-07-10 01:26:13 +0300

    🚀 Movie preview

--- 003-posts/src/App.jsx
+++ 003-posts/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Modal from "./components/Modal";
 
 function App() {
   //! The app named posts, but I will use movies api cuz this is the best one i found out there for what i want to do
@@ -33,17 +34,37 @@ function App() {
 
   useEffect(() => {
     const abortController = new AbortController();
+
     (async () => {
       if (searchQuery.length > 0) {
         await fetchMovies(abortController);
       }
     })();
     return () => {
-      console.log("cleanup");
       abortController.abort();
       setLoading(false);
     };
   }, [searchQuery]);
+
+  const [openedModal, setOpenedModal] = useState(false);
+  const toggleModal = () => {
+    setOpenedModal(!openedModal);
+  };
+
+  const [modalContent, setModalContent] = useState();
+  const handleModalContent = (content) => {
+    setModalContent(content);
+    toggleModal();
+  };
+
+  useEffect(() => {
+    if (openedModal) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [openedModal]);
+
   return (
     <>
       <div className="px-2  py-10 bg-slate-100 h-[100svh] overflow-auto">
@@ -64,33 +85,64 @@ function App() {
         {/* Results */}
         <div className="w-11/12 mx-auto">
           <div className="flex justify-center items-center flex-wrap">
-            {loading && (
+            {/* Loading */}
+            {loading && !!searchQuery.length && (
               <div className="flex justify-center items-center">
                 <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-slate-900"></div>
               </div>
             )}
 
+            {/* No results */}
+            {!loading && !movies.length && !!searchQuery.length && (
+              <div className="flex justify-center items-center">
+                <h3 className="text-xl font-bold text-slate-700">
+                  No results found
+                </h3>
+              </div>
+            )}
+
+            {/* no search query yet */}
+            {!searchQuery.length && !movies.length && (
+              <div className="flex justify-center items-center flex-col">
+                <div className="w-44 h-44 rounded-full overflow-hidden select-none pointer-events-none">
+                  <img
+                    className="w-full h-full object-contain mix-blend-normal"
+                    src="https://cdn.dribbble.com/userupload/6516616/file/original-9d21aa37c60b1789d0a075b2cd7c2672.png?compress=1&resize=752x564"
+                    alt=""
+                  />
+                </div>
+                <h3 className="text-md font-bold text-slate-700">
+                  Search for something
+                </h3>
+              </div>
+            )}
+
+            {/* Results */}
             {!!movies.length &&
               !loading &&
               movies.map((movie, index) => {
                 // check for the movie poster
                 if (movie.poster_path)
                   return (
-                    <div
-                      key={movie.id}
-                      className="bg-slate-200 rounded-xl shadow-md max-w-[120px] m-2"
-                    >
-                      <div className="flex w-[120px] h-[180px] justify-center items-center">
-                        <img
-                          src={img_base_url + movie.poster_path}
-                          alt={movie.name}
-                          className="rounded-xl shadow-lg w-full h-full object-cover"
-                        />
-                      </div>
-                      <div className="p-2 max-w-full">
-                        <h3 className="text-xs max-w-full min-h-[30px] line-clamp-2 font-bold text-slate-700">
-                          {movie.name || `Movie ${index + 1}`}
-                        </h3>
+                    <div key={movie.id}>
+                      <div
+                        onClick={() => {
+                          handleModalContent(movie);
+                        }}
+                        className="bg-slate-200 duration-300 hover:scale-105 cursor-pointer rounded-xl shadow-md max-w-[120px] m-2"
+                      >
+                        <div className="flex w-[120px] h-[180px] justify-center items-center">
+                          <img
+                            src={img_base_url + movie.poster_path}
+                            alt={movie.name}
+                            className="rounded-xl shadow-lg w-full h-full object-cover"
+                          />
+                        </div>
+                        <div className="p-2 max-w-full">
+                          <h3 className="text-xs max-w-full min-h-[30px] line-clamp-2 font-bold text-slate-700">
+                            {movie.name || `Movie ${index + 1}`}
+                          </h3>
+                        </div>
                       </div>
                     </div>
                   );
@@ -98,6 +150,10 @@ function App() {
           </div>
         </div>
       </div>
+      {/* Modal */}
+      {openedModal && (
+        <Modal toggleModal={toggleModal} content={modalContent} />
+      )}
     </>
   );
 }
---
+++ 003-posts/src/components/Modal.jsx
@@ -0,0 +1,58 @@
+export default function Modal({ toggleModal, content }) {
+  const img_base_url = "https://image.tmdb.org/t/p/w500";
+
+  return (
+    <div
+      onClick={(event) => {
+        if (event.target !== event.currentTarget) {
+          return;
+        }
+
+        toggleModal();
+      }}
+      className="fixed top-0 left-0 overflow-hidden w-full h-[100svh] flex items-end bg-[rgba(225,225,225,0.2)] backdrop-blur-sm cursor-pointer"
+    >
+      <div className="w-full relative h-1/2 flex justify-center items-center cursor-default rounded-t-2xl">
+        <div className="px-3 pt-5 absolute inset-0 z-10 w-full h-full overflow-hidden  bg-slate-200 rounded-t-2xl">
+          <div>
+            <div className="flex justify-center items-center flex-col pb-5">
+              <div className="w-full h-64 overflow-hidden rounded-t-xl">
+                <div className="absolute z-0 h-64 left-0 top-0 w-full bg-black">
+                  <img
+                    className="w-full h-full object-cover"
+                    src={img_base_url + content.backdrop_path}
+                    alt=""
+                  />
+                </div>
+                <div className="absolute flex justify-center items-center z-10 bg-[rgba(0,0,0,0.5)] backdrop-blur-sm h-64 top-0 left-0 w-full">
+                  <div className="w-36 shadow-2xl shadow-slate-800 overflow-hidden select-none rounded-lg pointer-events-none">
+                    <img
+                      className="w-full h-full object-conver mix-blend-normal"
+                      src={img_base_url + content.poster_path}
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+              <div>
+                <h3 className="text-2xl font-bold text-slate-700">
+                  {content.name || "Movie Name"}
+                </h3>
+                <p className="text-sm mt-2 text-slate-700 px-1 line-clamp-3">
+                  {content.overview}
+                </p>
+                <div className="mt-4">
+                  <button disabled className="px-3 py-2 disabled:bg-slate-400 disabled:cursor-not-allowed rounded-lg bg-slate-800 text-white shadow-2xl hover:shadow-none hover:bg-slate-600">
+                    <span className="pr-3">❤️</span>
+                    Add to favourite (soon maybe 🤷🏻)
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="absolute z-0 inset-0 -top-7 rounded-t-xl w-full h-full bg-black  blur-lg opacity-40"></div>
+      </div>
+    </div>
+  );
+}
