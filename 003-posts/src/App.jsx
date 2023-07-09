fd7bc1a13c2e0e00a85120fe5eeabbacfdd0d057
ahmedragab <ahmedragab20901@gmail.com>
2023-07-09 03:25:54 +0300

    🚀 Search done

--- 003-posts/src/App.jsx
+++ 003-posts/src/App.jsx
@@ -1,35 +1,105 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  //! The app named posts, but I will use movies api cuz this is the best one i found out there for what i want to do
 
+  const [searchQuery, setSearchQuery] = useState("");
+  const url = new URL("https://api.themoviedb.org/3/search/multi");
+  const api_key = "96118d622d2c41ec061099bbc178523d";
+  const img_base_url = "https://image.tmdb.org/t/p/w500";
+  url.search = new URLSearchParams({
+    api_key,
+    query: searchQuery,
+    include_adult: false,
+    language: "en-US",
+  }).toString();
+
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchMovies = async (abortController) => {
+    setLoading(true);
+    await fetch(url, {
+      signal: abortController.signal,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    (async () => {
+      if (searchQuery.length > 0) {
+        await fetchMovies(abortController);
+      }
+    })();
+    return () => {
+      console.log("cleanup");
+      abortController.abort();
+      setLoading(false);
+    };
+  }, [searchQuery]);
   return (
     <>
-      <div className='flex justify-center items-center'>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="px-2 py-10 bg-slate-100 h-[100svh] overflow-auto">
+        <div className="flex justify-center items-center">
+          <h2 className=" text-4xl font-extrabold text-slate-700">Search</h2>
+        </div>
+        <div className="my-5">
+          <div className="flex justify-center items-center">
+            <input
+              type="search"
+              className="w-11/12 p-2 border border-slate-300 rounded-md focus:outline-none focus:ring-2 focus:ring-slate-300 focus:border-transparent"
+              placeholder="Type to search..."
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </div>
+        </div>
+        {/* movies */}
+        <div>
+          <div className="flex justify-center items-center flex-wrap">
+            {loading && (
+              <div className="flex justify-center items-center">
+                <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-slate-900"></div>
+              </div>
+            )}
+
+            {!!movies.length &&
+              !loading &&
+              movies.map((movie, index) => {
+                // check for the movie poster
+                if (movie.poster_path)
+                  return (
+                    <div
+                      key={movie.id}
+                      className="bg-slate-200 rounded-xl shadow-md max-w-[120px] m-2"
+                    >
+                      <div className="flex justify-center items-center">
+                        <img
+                          src={img_base_url + movie.poster_path}
+                          alt={movie.name}
+                          className="rounded-xl shadow-lg"
+                        />
+                      </div>
+                      <div className="p-2 max-w-full">
+                        <h3 className="text-xs max-w-full min-h-[30px] line-clamp-2 font-bold text-slate-700">
+                          {movie.name || `Movie ${index + 1}`}
+                        </h3>
+                      </div>
+                    </div>
+                  );
+              })}
+          </div>
+        </div>
       </div>
-      <h1 className='font-mono text-4xl font-extrabold'>Vite + React</h1>
-      <div className="card">
-        <button className='px-5 py-2 rounded-xl border bg-blue-50 text-blue-600 active:scale-90 duration-300 hover:border-blue-600 select-none' onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p className='py-5'>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
